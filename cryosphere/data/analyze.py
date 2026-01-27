8e41716753e43943bf28e2b90c8be7cd6f3ed04b
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-01-27 17:19:21 +0100

    Debugging

--- cryosphere/data/analyze.py
+++ cryosphere/data/analyze.py
@@ -378,7 +378,7 @@ def compute_losses_argmin(rank, world_size, vae, segmenter, base_structure, path
         batch_predicted_images = renderer.apply_ctf(predicted_images, ctf, indexes)  # /dataset.f_std
         print("INDEXES SHAPE", indexes.shape)
         print(indexes)
-        _, images, _, _ , _ = dataset_images.__getitem__(indexes)
+        _, images, _, _ , _ = dataset_images.__getitem__(indexes.detach().cpu().numpy())
         rmsd, argmins, rmsd_non_mean = loss.calc_cor_loss(batch_predicted_images, images, mask_image)
 
         if rank == 0:
