2aeba12dde3e95f4d9fe4fbe04fd861cada70f5d
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2026-01-27 17:49:18 +0100

    Moving the images to gpu

--- cryosphere/data/analyze.py
+++ cryosphere/data/analyze.py
@@ -380,6 +380,7 @@ def compute_losses_argmin(rank, world_size, vae, segmenter, base_structure, path
         print("INDEXES SHAPE", indexes.shape)
         print(indexes)
         _, images, _, _ , _ = dataset_images[int(indexes[0].detach().cpu().numpy())]
+        images = images.to(rank)
         rmsd, argmins, rmsd_non_mean = loss.calc_cor_loss(batch_predicted_images, images, mask_image)
 
         if rank == 0:
